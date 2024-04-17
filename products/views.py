aee233f83a2b141cade576e5a0f79294586f4dbb
Nick8735 <nick8723@hotmail.co.uk>
2024-04-17 19:00:24 +0000

    Fixed data base

--- products/views.py
+++ products/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import Product  # Import the Product model
+from .models import Product
 
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'products/all_products.html', {'products': products})
 
 def wedding_products(request):
-    products = Product.objects.filter(category__name='wedding')
+    products = Product.objects.filter(category__friendly_name='Wedding')
     context = {'products': products}
     return render(request, 'products/wedding.html', context)
 
