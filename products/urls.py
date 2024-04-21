6ead3d500b1c8a2a1e6b8bc15f7390e298558c6e
Nick8735 <nick8723@hotmail.co.uk>
2024-04-21 19:23:29 +0000

    add more to DB

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),  # This should match the URL 'products/'
-    path('wedding/', views.wedding_products, name='wedding'),  # Assuming you have a view for wedding products
+  
     # Add more URL patterns as needed
 ]
--- products/views.py
+++ products/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from .models import Product
 
 def all_products(request):
+    products_w1 = Product.objects.filter(category__friendly_name='W1')
+    products_w2 = Product.objects.filter(category__friendly_name='W2')
     products = Product.objects.all()
     return render(request, 'products/all_products.html', {'products': products})
 
 def wedding_products(request):
-    products = Product.objects.filter(category__friendly_name='Wedding')
-    context = {'products': products}
-    return render(request, 'products/wedding.html', context)
-
+    products_w1 = Product.objects.filter(category__friendly_name='W1')
+    products_w2 = Product.objects.filter(category__friendly_name='W2')
+    categories = {'W1': products_w1, 'W2': products_w2}
+    return render(request, 'products/all_products.html', {'categories': categories})
