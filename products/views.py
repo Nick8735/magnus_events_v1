889b800991c0a895c23a12272c44f1f121c5822a
Nick8735 <nick8723@hotmail.co.uk>
2024-04-23 20:18:57 +0000

    added images

--- products/views.py
+++ products/views.py
@@ -2,17 +2,8 @@ from django.shortcuts import render
 from .models import Product
 
 def all_products(request):
-    # Retrieve all products
-    products = Product.objects.all()
+    # Retrieve all products ordered by SKU
+    products = Product.objects.order_by('sku')
     return render(request, 'products/all_products.html', {'products': products})
 
-def wedding_products(request):
-    # Filter products by category
-    products_w1 = Product.objects.filter(category__friendly_name='W1')
-    products_w2 = Product.objects.filter(category__friendly_name='W2')
-    products_w3 = Product.objects.filter(category__friendly_name='W3')
-    
-    # Create a dictionary to hold products for each category
-    categories = {'W1': products_w1, 'W2': products_w2, 'W3': products_w3}
-    
-    return render(request, 'products/wedding_products.html', {'categories': categories})
+
