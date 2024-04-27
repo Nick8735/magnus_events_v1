99a9a94183762e5f010d2eca74b608cc4f3d5fd6
Nick8735 <nick8723@hotmail.co.uk>
2024-04-27 12:49:54 +0000

    created product detail

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),  # This should match the URL 'products/'
-  
+    path('<product_id>', views.product_detail, name='product_detail'),
     # Add more URL patterns as needed
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def all_products(request):
     # Retrieve all products ordered by SKU
     products = Product.objects.order_by('sku')
+
+    context = {
+        'products': products,
+    }
     return render(request, 'products/all_products.html', {'products': products})
 
+def product_detail(request, product_id):
+    """ A view to show individual product details """
 
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/product_detail.html', context)
