d7e059bad24cffdf71eec09cb80b7fd90326dcd6
Nick8735 <nick8723@hotmail.co.uk>
2024-05-06 11:09:39 +0000

    added remove function

--- bag/views.py
+++ bag/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
-from django.contrib import messages
 
-from products.models import Product
-
+# Create your views here.
 
 def view_bag(request):
     """ A view that renders the bag contents page """
