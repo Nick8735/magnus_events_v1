dbc1ca5c52e06f6df95da010a917c583509f4f39
Nick8735 <nick8723@hotmail.co.uk>
2024-05-11 09:30:12 +0000

    added detail

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products'),  # This should match the URL 'products/'
-    path('<product_id>', views.product_detail, name='product_detail'),
-    # Add more URL patterns as needed
+    path('', views.all_products, name='products'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
