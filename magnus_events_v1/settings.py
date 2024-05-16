399fec8674cf42a6274aa9edc3bb55619899e9d2
Nick8735 <nick8723@hotmail.co.uk>
2024-05-16 13:31:05 +0000

    updated settings

--- magnus_events_v1/settings.py
+++ magnus_events_v1/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'c$&q^7+#j!c%d*-kqz*dm$s$h_oev((5w@z_)((c-i-s349^fc')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOPMENT')
+DEBUG = True
 
 ALLOWED_HOSTS = [ 
 'magnus-events-f6d64cf0d4be.herokuapp.com',
