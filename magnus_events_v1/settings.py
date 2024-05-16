e7297f8b8ac9dc04969ba1c1f7b49c39fad820d5
Nick8735 <nick8723@hotmail.co.uk>
2024-05-16 11:23:24 +0000

    updated seetings

--- magnus_events_v1/settings.py
+++ magnus_events_v1/settings.py
@@ -179,13 +179,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # Corrected spelling
-
-
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/workspace/magnus_events_v1/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
