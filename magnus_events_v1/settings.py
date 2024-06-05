a239682e9ca824b920ff934d91977973dd145a30
Nick8735 <nick8723@hotmail.co.uk>
2024-06-05 15:22:30 +0000

    added aws setting

--- magnus_events_v1/settings.py
+++ magnus_events_v1/settings.py
@@ -195,7 +195,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'magnus-events'
+    AWS_DEFAULT_ACL = None
+    AWS_STORAGE_BUCKET_NAME = 'magnus-events'    
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
