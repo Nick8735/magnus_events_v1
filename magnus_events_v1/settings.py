0300641d8df23496482d4cddec8aa3119aef1a94
Nick8735 <nick8723@hotmail.co.uk>
2024-06-05 15:32:55 +0000

    commented out in settings

--- magnus_events_v1/settings.py
+++ magnus_events_v1/settings.py
@@ -76,7 +76,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',  
+    #'allauth.account.middleware.AccountMiddleware',  
 ]
 
 ROOT_URLCONF = 'magnus_events_v1.urls'
